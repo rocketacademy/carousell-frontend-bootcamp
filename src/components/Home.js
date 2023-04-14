a2f539deb6fa4f95624c971359b61b566fd8929a
ivan89kiat <ivan89kiat@gmail.com>
2023-04-14 09:06:46 +0700

    auth0 implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import ListingPreviewList from "./ListingPreviewList";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = () => {
+  const { logout } = useAuth0();
   return (
     <div>
       <Link to="/listings/new">Sell</Link>
       <br />
+
+      <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Log Out
+      </button>
       <br />
       <ListingPreviewList />
     </div>
