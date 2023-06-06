6cd29456d8ed4be2cedefc3f72727fa4e9f45675
Syafiq Ismail <macamyesjer@gmail.com>
2023-06-06 18:42:48 +0800

    finish exercise

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import { Outlet } from "react-router-dom";
 
 import "./App.css";
 import logo from "../logo.png";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
 
 const App = () => (
   <div className="App">
     <header className="App-header">
+      <LoginButton />
+      <LogoutButton />
       <img src={logo} className="App-logo" alt="logo" />
       <Outlet />
     </header>
