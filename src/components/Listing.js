acd198d754d2fe436f555462eac66c68c8888290
JerryTohHS <jerrytoh93@gmail.com>
2023-08-21 22:04:08 +0800

    Frontend

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
@@ -9,6 +10,9 @@ import { BACKEND_URL } from "../constants.js";
 const Listing = () => {
   const [listingId, setListingId] = useState();
   const [listing, setListing] = useState({});
+
+  const { isAuthenticated, getAccessTokenSilently, loginWithRedirect, user } =
+    useAuth0();
 
   useEffect(() => {
     // If there is a listingId, retrieve the listing data
@@ -36,10 +40,27 @@ const Listing = () => {
     }
   }
 
-  const handleClick = () => {
-    axios.put(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
-      setListing(response.data);
+  const handleClick = async () => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+      return;
+    }
+
+    const accessToken = await getAccessTokenSilently({
+      audience: "https://carousell/api",
+      scope: "read:current_user",
     });
+
+    const response = await axios.put(
+      `${BACKEND_URL}/listings/${listingId}`,
+      { buyerEmail: user.email },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    setListing(response.data);
   };
 
   return (
