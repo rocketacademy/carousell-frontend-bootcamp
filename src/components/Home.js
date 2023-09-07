ff31d0eadc61a1f55dc69f1489297bcf033e15a3
kaucers <jkarinayap@gmail.com>
2023-09-08 01:24:26 +0800

    carousell frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
-import ListingPreviewList from "./ListingPreviewList";
+import ListingPreviewList from './ListingPreviewList';
 
 const Home = () => {
+  const { loginWithRedirect, logout, isAuthenticated, isLoading, user } =
+    useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div>
-      <Link to="/listings/new">Sell</Link>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <br />
+      <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Log Out
+      </button>
+      <br />
+      <Link to='/listings/new'>Sell</Link>
       <br />
       <br />
       <ListingPreviewList />
+      {isAuthenticated && (
+        <div>
+          <h2>{user.nickname}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
     </div>
   );
 };
