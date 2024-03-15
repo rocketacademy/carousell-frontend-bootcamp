00753e29ef406ec3cea33bf05dd9a1d98df2c372
qiannnly <qianlinggg@gmail.com>
2024-03-15 13:31:26 +0800

    added login and logout buttons

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ import { Outlet } from "react-router-dom";
 
 import "./App.css";
 import logo from "../logo.png";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
 
 const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+      <LoginButton />
+      <LogoutButton />
       <Outlet />
     </header>
   </div>
