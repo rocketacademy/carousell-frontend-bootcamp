fba49bf32e6e9587eade4b421c8e9c45cc9f1da0
Mannushka <zaimar1910@gmail.com>
2024-03-15 22:27:16 +0800

    import and paste logout button

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { Outlet } from "react-router-dom";
 
 import "./App.css";
 import logo from "../logo.png";
+import LogoutButton from "./LogoutButton";
 
 const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <Outlet />
+      <LogoutButton />
     </header>
   </div>
 );
