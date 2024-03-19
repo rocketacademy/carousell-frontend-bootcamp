177e2e4619f81eb590b92df166ede56736853b47
magiicloud <hongyun_rc@outlook.com>
2024-03-19 21:09:39 +0800

    final version

--- src/index.js
+++ src/index.js
@@ -6,22 +6,34 @@ import App from "./components/App";
 import Home from "./components/Home";
 import NewListingForm from "./components/NewListingForm";
 import Listing from "./components/Listing";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      {/* Route that provides base app UI */}
-      <Route path="/" element={<App />}>
-        {/* Route that renders home content */}
-        <Route index element={<Home />} />
-        {/* Route that renders new listing form */}
-        <Route path="listings/new" element={<NewListingForm />} />
-        {/* Route that renders individual listings */}
-        <Route path="listings/:listingId" element={<Listing />} />
-        {/* Route that matches all other paths */}
-        <Route path="*" element={"Nothing here!"} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <Auth0Provider
+    domain="dev-sz0chkcuth5rdizf.us.auth0.com"
+    clientId="NtqooOHVFq1Ka1alHcsey4bc7R4uYikk"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: "https://carousell/api",
+      scope:
+        "openid profile email read:current_user update:current_user_metadata",
+    }}
+  >
+    <BrowserRouter>
+      <Routes>
+        {/* Route that provides base app UI */}
+        <Route path="/" element={<App />}>
+          {/* Route that renders home content */}
+          <Route index element={<Home />} />
+          {/* Route that renders new listing form */}
+          <Route path="listings/new" element={<NewListingForm />} />
+          {/* Route that renders individual listings */}
+          <Route path="listings/:listingId" element={<Listing />} />
+          {/* Route that matches all other paths */}
+          <Route path="*" element={"Nothing here!"} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Auth0Provider>
 );
